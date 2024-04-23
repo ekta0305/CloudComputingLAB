269de7d7d647e961c24ff34e0c00fd3284a7f3de
EKTA DHAKE <ektadhake@gmail.com>
2024-04-23 16:08:31 +0530

    docker

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+
+  console.log("Current User", user);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {isAuthenticated && <h3>Hello {user.name}</h3>}
+        {isAuthenticated ? (
+          <button onClick={e => logout()}>Logout</button> 
+        ) :  (
+          <button onClick={e => loginWithRedirect()}>
+            Login with Redirect
+          </button>
+        )}
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
